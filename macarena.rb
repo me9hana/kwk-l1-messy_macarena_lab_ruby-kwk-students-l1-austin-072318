adfc65809118e975f42aee7330d340197a424b47
me9hana <meghanagoli@gmail.com>
2018-07-23 19:19:37 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -9,13 +9,13 @@ puts ""
 puts "Step 1: Right arm out in front of you, palm facing down."
 
 # Step 2: FIXME
-puts 
+puts "Step 2: Left arm out in front of you, palm facing down."
 
 # Step 3: FIXME
-puts "Step 3: Place your right hand on your left shoulder."
+puts "Step 3: "
 
 # Step 4: FIXME
-puts "Step 4: Turn your left palm up."
+puts "Step 4: "
 
 # Step 5: FIXME
 puts "Step 5: Bring your right hand up and around to the back of your head."
